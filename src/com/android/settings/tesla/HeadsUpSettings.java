i9500/android_packages_apps_Settings

Settings: Improve HeadsUp [2/2]
Just a rework of Setting entries

Change-Id: I3820ee56fef156d7f6f30c4127309dcb6ffd1863

src/com/android/settings/headsup/HeadsUpSettings.java
@@ -83,7 +83,6 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     private Map<String, Package> mBlacklistPackages;
 
     private BaseSystemSettingSwitchBar mEnabledSwitch;
-    private boolean mLastEnabledState;
 
     private ViewGroup mPrefsContainer;
     private View mDisabledText;
@@ -153,7 +152,7 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
         super.onStart();
         final SettingsActivity activity = (SettingsActivity) getActivity();
         mEnabledSwitch = new BaseSystemSettingSwitchBar(activity, activity.getSwitchBar(),
-                Settings.System.HEADS_UP_NOTIFICATION, true, this);
+                Settings.System.HEADS_UP_USER_ENABLED, true, this);
     }
 
     @Override
@@ -431,17 +430,27 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
         Settings.System.putString(getContentResolver(), setting, value);
     }
 
+    private boolean getUserHeadsUpState() {
+         return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.HEADS_UP_USER_ENABLED,
+                Settings.System.HEADS_UP_USER_ON,
+                UserHandle.USER_CURRENT) != 0;
+    }
+
+    private void setUserHeadsUpState(int val) {
+         Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.HEADS_UP_USER_ENABLED,
+                val, UserHandle.USER_CURRENT);
+    }
+
     private void updateEnabledState() {
-        boolean enabled = Settings.System.getInt(getContentResolver(),
-                Settings.System.HEADS_UP_NOTIFICATION, 1) != 0;
-        mPrefsContainer.setVisibility(enabled ? View.VISIBLE : View.GONE);
-        mDisabledText.setVisibility(enabled ? View.GONE : View.VISIBLE);
+        mPrefsContainer.setVisibility(getUserHeadsUpState() ? View.VISIBLE : View.GONE);
+        mDisabledText.setVisibility(getUserHeadsUpState() ? View.GONE : View.VISIBLE);
     }
 
     @Override
     public void onEnablerChanged(boolean isEnabled) {
-        mLastEnabledState = Settings.System.getInt(getContentResolver(),
-                Settings.System.HEADS_UP_NOTIFICATION, 1) != 0;
+        setUserHeadsUpState(getUserHeadsUpState() ? 1 : 0);
         updateEnabledState();
     }
 

TeslaOS/-android_packages_apps_Settings

Signed-off-by: NoelMacwan <yulespencer@gmail.com>

src/com/android/settings/tesla/HeadsUpSettings.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -78,13 +78,13 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     private Preference mAddDndPref;
     private Preference mAddBlacklistPref;
 
+    private Context mContext;
     private String mDndPackageList;
     private String mBlacklistPackageList;
     private Map<String, Package> mDndPackages;
     private Map<String, Package> mBlacklistPackages;
 
     private BaseSystemSettingSwitchBar mEnabledSwitch;
-    private boolean mLastEnabledState;
 
     private ViewGroup mPrefsContainer;
     private View mDisabledText;
@@ -92,6 +92,7 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = getActivity();
         // Get launch-able applications
         addPreferencesFromResource(R.xml.heads_up_settings);
         mPackageManager = getPackageManager();
@@ -154,7 +155,7 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
         super.onStart();
         final SettingsActivity activity = (SettingsActivity) getActivity();
         mEnabledSwitch = new BaseSystemSettingSwitchBar(activity, activity.getSwitchBar(),
-                Settings.TESLA.HEADS_UP_NOTIFICATION, true, this);
+                Settings.TESLA.HEADS_UP_USER_ENABLED, true, this);
     }
 
     @Override
@@ -432,17 +433,27 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
         Settings.TESLA.putString(getContentResolver(), setting, value);
     }
 
+    private boolean getUserHeadsUpState() {
+         return Settings.TESLA.getIntForUser(mContext.getContentResolver(),
+                Settings.TESLA.HEADS_UP_USER_ENABLED,
+                Settings.TESLA.HEADS_UP_USER_ON,
+                UserHandle.USER_CURRENT) != 0;
+    }
+
+    private void setUserHeadsUpState(int val) {
+         Settings.TESLA.putIntForUser(mContext.getContentResolver(),
+                Settings.TESLA.HEADS_UP_USER_ENABLED,
+                val, UserHandle.USER_CURRENT);
+    }
+
     private void updateEnabledState() {
-        boolean enabled = Settings.TESLA.getInt(getContentResolver(),
-                Settings.TESLA.HEADS_UP_NOTIFICATION, 1) != 0;
-        mPrefsContainer.setVisibility(enabled ? View.VISIBLE : View.GONE);
-        mDisabledText.setVisibility(enabled ? View.GONE : View.VISIBLE);
+        mPrefsContainer.setVisibility(getUserHeadsUpState() ? View.VISIBLE : View.GONE);
+        mDisabledText.setVisibility(getUserHeadsUpState() ? View.GONE : View.VISIBLE);
     }
 
     @Override
     public void onEnablerChanged(boolean isEnabled) {
-        mLastEnabledState = Settings.TESLA.getInt(getContentResolver(),
-                Settings.TESLA.HEADS_UP_NOTIFICATION, 1) != 0;
+        setUserHeadsUpState(getUserHeadsUpState() ? 1 : 0);
         updateEnabledState();
     }
 